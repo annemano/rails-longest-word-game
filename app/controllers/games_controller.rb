8ce13face7ce1f883d61f27a31a85f17627e3b3b
Anne-Marie Nault <anne.ma@gmail.com>
2021-05-10 16:36:55 -0400

    implemented method to calculate total score for each session and refactored code for #score method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,16 +11,33 @@ class GamesController < ApplicationController
   def score
     @word = params[:word]
     @letters = params[:letters]
-    # check if word letters can be built from the original grid
+    # check if letters of the word are available in the grid
+    @in_grid = word_in_grid?
+    # check if valid english word
+    @valid = valid_english_word?
+    # calculate total score for this session
+    @user_score = total_score
+  end
+
+  private
+
+  def word_in_grid?
     letter_hash = @letters.split.each_with_object(Hash.new(0)) { |v, h| h[v] += 1 }
     word_hash = @word.upcase.split('').each_with_object(Hash.new(0)) { |v, h| h[v] += 1 }
     reviewed_word = word_hash.select { |key, value| value <= letter_hash[key] }
-    @grid = word_hash == reviewed_word
-    # check if valid english word
+    word_hash == reviewed_word
+  end
+
+  def valid_english_word?
     word_serialized = URI.open("https://wagon-dictionary.herokuapp.com/#{params[:word]}").read
-    @found = JSON.parse(word_serialized)['found']
-    raise
-    # calculate score
-    # @score += @word.length if @grid && @found
+    JSON.parse(word_serialized)['found']
+  end
+
+  def total_score
+    session[:score] = 0 if session[:score].nil?
+    # add points to the total score if valid word
+    session[:score] += @word.length if @in_grid && @valid
+    # return total score
+    session[:score]
   end
 end
